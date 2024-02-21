4617fb96a9256d3b9c7b868c97a43f9e78d43724
Agreatdayforfreedom <matias.bertoni2002@gmail.com>
2024-02-21 11:58:04 -0300

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <div>
       <div></div>
       <h1>Vite + React</h1>
       <div className="card">
